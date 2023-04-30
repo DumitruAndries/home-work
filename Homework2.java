f624dc71e421a626647edde352a6bb5616c59fb9
DumitruAndries <131201434+DumitruAndries@users.noreply.github.com>
2023-04-30 21:36:10 +0300

    Add files via upload

--- Homework2.java
+++ Homework2.java
@@ -5,11 +5,11 @@ public class Homework2 {
         System.out.println("27.04.23 номер 2");
 
         int n = 365;
-        int e = n%10;
+        int e = n % 10;
         System.out.println(e);
-        int d = n/10%10;
+        int d = n / 10 % 10;
         System.out.println(d);
-        int s = n/100;
+        int s = n / 100;
         System.out.println(s);
 
     }
